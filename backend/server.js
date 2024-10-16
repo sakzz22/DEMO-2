ed6ce1987076f397bb6e2d9756678f7f0f0c3cd1
Admin <sakshamwork2222@gmail.com>
2024-10-16 22:05:12 +0530

    changes donee

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use("/api/posts",postRoutes);
 app.use("/api/messages",messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist/index.html")));
 
     app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
