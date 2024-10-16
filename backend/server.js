eb8e12183c3ac40e767c4eaedb02f00ba12b7664
Admin <sakshamwork2222@gmail.com>
2024-10-16 22:37:05 +0530

    changes donee

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,6 @@ app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/messages",messageRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist/index.html")));
-
-//     app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
-// 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
